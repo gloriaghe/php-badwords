079aedfefddbe400d48258d86c1a13b0a60193b1
Gloria <gloghe@yahoo.it>
2022-06-27 10:22:52 +0200

    aggiunta stile colore

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 $word = $_GET["name"];
 $text= 'Cat ipsum dolor sit amet, ut ipsa. Minima laboris proident. Perspiciatis quia, quis cat. Excepteur. Quam minima adipisicing ab but nihil velit. Quaerat mollit do lorem irure and incididunt. Dolorem sint. Ab incidunt aut or qui but lorem. Dolor. Velitesse odit cat. Nulla enim amet exercitationem cat quis quam. Nequeporro ab. Et irure. Quaerat doloremque numquam or eius esse sit for mollit. Dolores cupidatat eius consectetur or omnis. Beatae nesciunt or dolore si so architecto for aperiam cat dolor. Aute aliquip nulla unde laboris. Si iure for incididunt but aperiam but voluptate or modi ad cat. Natus molestiae, qui, ad autem ipsa for ipsa cat dolor.';
-$replaced_text = str_replace($word, '***', $text, $replaced_text );
+// $replaced_text = str_replace($word, '***', $text, $replaced_text );
+$replaced_text = str_replace($word, '<span>***</span>', $text, $replaced_text );
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +12,13 @@ $replaced_text = str_replace($word, '***', $text, $replaced_text );
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- gestiamo colore css -->
+    <style>
+        span{
+            color: red;
+        }
+    </style>
 </head>
 <body>
     <div>
